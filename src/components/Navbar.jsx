cfb7ff9689f30cc2be4bce0573c6fa5f3f92cd78
omersb <omersaidbuldukk@gmail.com>
2022-12-29 19:47:53 +0300

    firebase added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
 	const navigate = useNavigate();
-	// const currentUser = { displayName: "felix franko" };
 	const currentUser = false;
 
 	return (
